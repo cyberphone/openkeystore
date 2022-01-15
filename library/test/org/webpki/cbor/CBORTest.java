846546003905cdb340dff56f797bb36fe0f904fa
Anders Rundgren <anders.rundgren.net@gmail.com>
2022-01-15 17:50:09 +0100

    Update CBORTest.java

--- library/test/org/webpki/cbor/CBORTest.java
+++ library/test/org/webpki/cbor/CBORTest.java
@@ -16,6 +16,7 @@
  */
 package org.webpki.cbor;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -244,6 +245,22 @@ public class CBORTest {
             checkException(e, "Unsupported tag: " + hex.substring(0, 2).toLowerCase());
         }
     }
+    
+    void floatTest(String asText, String hex) {
+        floatTest(asText, hex, 0);
+    }
+    void floatTest(String asText, String hex, int mustFail) {
+        double v = Double.valueOf(asText);
+        try {
+            CBORObject cborObject = parseCborHex(hex);
+            assertFalse("Double should fail", mustFail == 1);
+            Double d = cborObject.getFloatingPoint();
+            assertTrue("Equal d=" + d + " v=" + v, (d.compareTo(v)) == 0 ^ (mustFail != 0));
+        } catch (Exception e) {
+            assertTrue("Ok fail", mustFail != 0);
+            checkException(e, "Non-deterministic encoding of floating point value");
+        }
+    }
 
     @Test
     public void assortedTests() throws Exception {
@@ -381,6 +398,54 @@ public class CBORTest {
         
         unsupportedTag("C07819323032312D30352D30315430363A33373A35352B30313A3030");
         unsupportedTag("1C");
+        
+        // These numbers are supposed to be tie-breakers...
+        floatTest("10.55999755859375",          "FA4128F5C0");
+        floatTest("-1.401298464324817e-45",     "FA80000001");
+        floatTest("-5.9604644775390625e-8",     "F98001");
+
+        floatTest("-2.9387358770557184e-39",    "FBB7EFFFFFFFFFFFFF");
+        floatTest("-2.9387358770557188e-39",    "FA80200000");
+
+        floatTest("-5.8774717541114375e-39",    "FA80400000");
+        floatTest("-1.1754943508222875e-38",    "FA80800000");
+        floatTest("-3.1691265005705735e+29",    "FAF0800000");
+        floatTest("-2.076918743413931e+34",     "FAF8800000");
+        floatTest("-5.3169119831396635e+36",    "FAFC800000");
+        floatTest("-2.1267647932558654e+37",    "FAFD800000");
+        
+        floatTest("3.4028234663852886e+38",     "FA7F7FFFFF");
+        floatTest("3.4028234663852889e+38",     "FB47EFFFFFE0000001");
+
+        floatTest("-8.507059173023462e+37",     "FAFE800000");
+        floatTest("-3.090948894593554e+30",     "FAF21C0D94");
+        floatTest("10.559999942779541",         "FB40251EB850000000");
+        floatTest("10.559998512268066",         "FA4128F5C1");
+        floatTest("1.0e+48",                    "FB49E5E531A0A1C873");
+        floatTest("18440.0",                    "FA46901000");
+        floatTest("18448.0",                    "F97481");
+        floatTest("3.0517578125e-5",            "F90200");
+        floatTest("3.057718276977539e-5",       "F90201");
+        floatTest("6.097555160522461e-5",       "F903FF");
+        floatTest("3.0547380447387695e-5",      "FA38002000");
+        floatTest("3.0584633350372314e-5",      "FA38004800");
+        floatTest("5.9604644775390625e-8",      "F90001");
+        floatTest("5.960465188081798e-8",       "FA33800001");
+        
+        floatTest("65504.0",                    "F97BFF");
+        floatTest("65504.00390625",             "FA477FE001");
+        floatTest("65505.0",                    "FA477FE100");
+
+        floatTest("65536.0",                    "FA47800000");
+        floatTest("NaN",                        "F97E00");
+        floatTest("Infinity",                   "F97C00");
+        floatTest("-Infinity",                  "F9FC00");
+        floatTest("0.0",                        "F90000");
+        floatTest("-0.0",                       "F98000");
+        
+        floatTest("NaN",                        "FA80000000",           1);
+        floatTest("NaN",                        "FB8000000000000000",   1);
+        floatTest("65504.00390625",             "F97BFF",               2);
     }
  
     public static boolean compareKeyId(byte[] keyId, byte[] optionalKeyId) {
