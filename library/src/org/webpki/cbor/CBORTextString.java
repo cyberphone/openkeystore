d0084e841a3f83cff5324c5854fa0f4b0080a799
Anders Rundgren <anders.rundgren.net@gmail.com>
2022-02-20 15:47:11 +0100

    Update CBORTextString.java

--- library/src/org/webpki/cbor/CBORTextString.java
+++ library/src/org/webpki/cbor/CBORTextString.java
@@ -49,12 +49,12 @@ public class CBORTextString extends CBORObject {
     // JavaScript/JSON compatible escape character support
     static final char[] SPECIAL_CHARACTERS = {
     //  00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F
-         1 ,  1 ,  1 ,  1 ,  1 ,  1 , 01,   1 , 'b', 't', 'n',  1 , 'f', 'r',  1 ,  1 ,
-         1 ,  1 ,  1 ,  1 ,  1 ,  1 , 01,   1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,
-         0 ,  0 , '"',  0 ,  0 ,  0 ,  0,   0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
-         0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0,   0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
-         0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0,   0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
-         0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0,   0 ,  0 ,  0 ,  0 ,  0 , '\\'};
+         1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 , 'b', 't', 'n',  1 , 'f', 'r',  1 ,  1 ,
+         1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,
+         0 ,  0 , '"',  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
+         0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
+         0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
+         0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 , '\\'};
     
     @Override
     void internalToString(CBORObject.DiagnosticNotation cborPrinter) {
