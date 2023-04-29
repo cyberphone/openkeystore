d1203b226dabf05362a8fff36783046cff42be7a
Anders Rundgren <anders.rundgren.net@gmail.com>
2023-04-29 17:12:11 +0200

    Update CBORX509Signer.java

--- library/src/org/webpki/cbor/CBORX509Signer.java
+++ library/src/org/webpki/cbor/CBORX509Signer.java
@@ -104,7 +104,7 @@ public class CBORX509Signer extends CBORSigner {
     void additionalItems(CBORMap signatureObject) {
         // X509 signatures mandate a certificate path.
         signatureObject.set(CERT_PATH_LABEL, 
-                                  CBORCryptoUtils.encodeCertificateArray(signer.getCertificatePath()));
+                            CBORCryptoUtils.encodeCertificateArray(signer.getCertificatePath()));
         // Key IDs are always rejected.
         CBORCryptoUtils.rejectPossibleKeyId(optionalKeyId);
     }
