f073269f83284e8dcf19a3b71b3bdc604c3b5179
Anders Rundgren <anders.rundgren.net@gmail.com>
2024-08-29 20:04:35 +0200

    Improved validation text

--- library/generators/cbor_api_demo/SignatureDemo.java
+++ library/generators/cbor_api_demo/SignatureDemo.java
@@ -19,15 +19,15 @@ public class SignatureDemo {
     static final byte[] HMAC_KEY = HexaDecimal.decode(
             "7fdd851a3b9d2dafc5f0d00030e22b9343900cd42ede4948568a4a2ee655291a");
     
-    static final CBORInt HELLO_LABEL     = new CBORInt(1);
-    static final CBORInt ARRAY_LABEL     = new CBORInt(2);
-    static final CBORInt SIGNATURE_LABEL = new CBORInt(-1);
+    static final CBORInt GREETINGS_LABEL  = new CBORInt(1);
+    static final CBORInt OTHER_DATA_LABEL = new CBORInt(2);
+    static final CBORInt SIGNATURE_LABEL  = new CBORInt(-1);
     
     public static void main(String[] args) {
         // Create CBOR data to be signed.
         CBORMap dataToBeSigned = new CBORMap()
-            .set(HELLO_LABEL, new CBORString("Hello Signed CBOR World!"))
-            .set(ARRAY_LABEL, new CBORArray()
+            .set(GREETINGS_LABEL, new CBORString("Hello Signed CBOR World!"))
+            .set(OTHER_DATA_LABEL, new CBORArray()
                 .add(new CBORFloat(-4.5))
                 .add(new CBORBoolean(true)));
         
@@ -45,7 +45,6 @@ new CborDocumentLog(args[0], args[1], "#sample.program#");
 //@end@
 
         // Fetch a map item.
-        String greatings = decodedCbor.get(HELLO_LABEL).getString();
-        System.out.println(greatings);
+        System.out.println(decodedCbor.get(GREETINGS_LABEL).getString());
     }
 }
