2f7ed24f52692122eccccd752a7297cdf5ada939
Anders Rundgren <anders.rundgren.net@gmail.com>
2025-12-18 21:52:15 +0100

    formatting...

--- library/src/org/webpki/cbor/CBORUtil.java
+++ library/src/org/webpki/cbor/CBORUtil.java
@@ -19,45 +19,46 @@ public class CBORUtil {
     /**
      * Create <code>DateTime</code> object.
      * 
-<div style='margin-top:0.5em'>
-This method creates a date/time string in the ISO format described in section 5.6&nbsp;of 
-[<a href='https://www.rfc-editor.org/rfc/rfc3339.html#section-5.6' class='webpkilink'>RFC3339</a>].
-The string is subsequently wrapped in a {@link CBORString} object.</div>
-<div style='margin-top:0.5em'>
-If the <code>instant</code> object is not within
-the range <code style='white-space:nowrap'>"0000-01-01T00:00:00Z"</code> to
-<code style='white-space:nowrap'>"9999-12-31T23:59:59Z"</code>,
-a {@link CBORException} is thrown .</div>
-<div style='margin-top:0.5em'>
-If <code>millis</code> is <code>true</code> the date/time string will feature
-milliseconds (<code>.nnn</code>) as well.</div>
-<div style='margin-top:0.5em'>Sample code:</div>
-<div style='margin:0.3em 0 0 1.2em'><code>CBORString iso = CBORUtil.createDateTime(Instant.now(), true, false);<br>
-System.out.println(iso.toString());<br>
-<span style='color:#007fdd'>"2025-12-05T13:55:42.418+01:00"</span></code></div>
-@see CBORObject#getDateTime()
-@param instant Time source object.
-@param millis <div style='margin-left:2em'>
-If <code>millis</code> is <code>true</code>,
-the milliseconds of the <code>instant</code> object will be
-featured in the created time object.  Note: if the millisecond
-part of the <code>instant</code> object is zero,
-<code>millis</code> is considered to be <code>false</code>.
-<div style='margin-top:0.5em'>If <code>millis</code> is
-<code>false</code>, the millisecond part of the <code>instant</code>
-object will not be used, but may after <i>rounding</i>,
-add a second to the created time object.
-Rounding is performed for milliseconds <code>&ge;500</code>,
-and always in the <i>positive</i> direction.</div><div>
-@param utc <div style='margin-left:2em'>
-If <code>utc</i></code> is <code>true</code>,
-the <code>UTC</code> time zone (denoted by a terminating <code>Z</code>) will be used,
-else the local time followed by the <code>UTC</code> offset
-(<code>&plusmn;hh:mm</code>) will be used.</div>
+     * <div style='margin-top:0.5em'>
+     * This method creates a date/time string in the ISO format described in section 5.6&nbsp;of 
+     * [<a href='https://www.rfc-editor.org/rfc/rfc3339.html#section-5.6' class='webpkilink'>RFC3339</a>].
+     * The string is subsequently wrapped in a {@link CBORString} object.</div>
+     * <div style='margin-top:0.5em'>
+     * If the <code>instant</code> object is not within
+     * the range <code style='white-space:nowrap'>"0000-01-01T00:00:00Z"</code> to
+     * <code style='white-space:nowrap'>"9999-12-31T23:59:59Z"</code>,
+     * a {@link CBORException} is thrown .</div>
+     * <div style='margin-top:0.5em'>
+     * If <code>millis</code> is <code>true</code> the date/time string will feature
+     * milliseconds (<code>.nnn</code>) as well.</div>
+     * <div style='margin-top:0.5em'>Sample code:</div>
+     * <div style='margin:0.3em 0 0 1.2em'>
+     * <code>CBORString iso = CBORUtil.createDateTime(Instant.now(), true, false);<br>
+     * System.out.println(iso.toString());<br>
+     * <span style='color:#007fdd'>"2025-12-05T13:55:42.418+01:00"</span></code></div>
+     * @see CBORObject#getDateTime()
+     * @param instant Time source object.
+     * @param millis <div style='margin-left:2em'>
+     * If <code>millis</code> is <code>true</code>,
+     * the milliseconds of the <code>instant</code> object will be
+     * featured in the created time object.  Note: if the millisecond
+     * part of the <code>instant</code> object is zero,
+     * <code>millis</code> is considered to be <code>false</code>.
+     * <div style='margin-top:0.5em'>If <code>millis</code> is
+     * <code>false</code>, the millisecond part of the <code>instant</code>
+     * object will not be used, but may after <i>rounding</i>,
+     * add a second to the created time object.
+     * Rounding is performed for milliseconds <code>&ge;500</code>,
+     * and always in the <i>positive</i> direction.</div></div>
+     * @param utc <div style='margin-left:2em'>
+     * If <code>utc</code> is <code>true</code>,
+     * the <code>UTC</code> time zone (denoted by a terminating <code>Z</code>) will be used,
+     * else the local time followed by the <code>UTC</code> offset
+     * (<code>&plusmn;hh:mm</code>) will be used.</div>
 
-@return {@link CBORString}
-@throws CBORException
-    */
+     * @return {@link CBORString}
+     * @throws CBORException
+     */
     public static CBORString createDateTime(Instant instant, boolean millis, boolean utc) {
         // Verify that time is within the permitted range.
         long instantMillis = instantDateTimeToMillisCheck(instant);
@@ -80,40 +81,43 @@ else the local time followed by the <code>UTC</code> offset
     /**
      * Create <code>EpochTime</code> object.
      * 
-<div style='margin-top:0.5em'>
-This method creates an Epoch
-[<a href='https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/V1_chap04.html#tag_04_19'
- class='webpkilink'>TIME</a>] time stamp.</div>
-<div style='margin-top:0.5em'>
-If the <code>instant</code> object is not within
-the range <span style='white-space:nowrap'><code>0</code> (<code>"1970-01-01T00:00:00Z"</code>)</span> to
-<span style='white-space:nowrap'><code>253402300799</code> (<code>"9999-12-31T23:59:59Z"</code>)</span>,
-a {@link CBORException} is thrown.</div>
-<div style='margin-top:0.5em'>
-If <code>millis</code> is <code>true</code> a {@link CBORFloat}
-object holding seconds with a milliseconds fraction will be created,
-else a {@link CBORInt} object holding seconds will be created.</div>
-<div style='margin-top:0.5em'>Sample code:</div>
-<div style='margin:0.3em 0 0 1.2em'><code>CBORObject epoch = CBORUtil.createEpochTime(Instant.now(), false);<br>
-System.out.println(epoch.toString());<br>
-<span style='color:#007fdd'>1764939916</span></code></div>
-@see CBORObject#getEpochTime()
-@param instant Time source object.
-@param millis <div style='margin-left:2em'>
-If <code>millis</code> is <code>true</code>,
-the milliseconds of the <code>instant</code> object will be
-featured in the created time object.  Note: if the millisecond
-part of the <code>instant</code> object is zero,
-<code>millis</code> is considered to be <code>false</code>.
-<div style='margin-top:0.5em'>If <code>millis</code> is
-<code>false</code>, the millisecond part of the <code>instant</code>
-object will not be used, but may after <i>rounding</i>,
-add a second to the created time object.
-Rounding is performed for milliseconds <code>&ge;500</code>.</div><div>
-
-@return {@link CBORObject}
-@throws CBORException
-    */
+     * <div style='margin-top:0.5em'>
+     * This method creates an Epoch
+     * [<a href='https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/V1_chap04.html#tag_04_19'
+     * class='webpkilink'>TIME</a>] time stamp.</div>
+     * <div style='margin-top:0.5em'>
+     * If the <code>instant</code> object is not within
+     * the range <span style='white-space:nowrap'><code>0</code>
+     * (<code>"1970-01-01T00:00:00Z"</code>)</span> to
+     * <span style='white-space:nowrap'><code>253402300799</code> 
+     * (<code>"9999-12-31T23:59:59Z"</code>)</span>,
+     * a {@link CBORException} is thrown.</div>
+     * <div style='margin-top:0.5em'>
+     * If <code>millis</code> is <code>true</code> a {@link CBORFloat}
+     * object holding seconds with a milliseconds fraction will be created,
+     * else a {@link CBORInt} object holding seconds will be created.</div>
+     * <div style='margin-top:0.5em'>Sample code:</div>
+     * <div style='margin:0.3em 0 0 1.2em'>
+     * <code>CBORObject epoch = CBORUtil.createEpochTime(Instant.now(), false);<br>
+     * System.out.println(epoch.toString());<br>
+     * <span style='color:#007fdd'>1764939916</span></code></div>
+     * @see CBORObject#getEpochTime()
+     * @param instant Time source object.
+     * @param millis <div style='margin-left:2em'>
+     * If <code>millis</code> is <code>true</code>,
+     * the milliseconds of the <code>instant</code> object will be
+     * featured in the created time object.  Note: if the millisecond
+     * part of the <code>instant</code> object is zero,
+     * <code>millis</code> is considered to be <code>false</code>.
+     * <div style='margin-top:0.5em'>If <code>millis</code> is
+     * <code>false</code>, the millisecond part of the <code>instant</code>
+     * object will not be used, but may after <i>rounding</i>,
+     * add a second to the created time object.
+     * Rounding is performed for milliseconds <code>&ge;500</code>.</div></div>
+     * 
+     * @return {@link CBORObject}
+     * @throws CBORException
+     */
     public static CBORObject createEpochTime(Instant instant, boolean millis) {
         long instantMillis = instant.toEpochMilli();
         if (instantMillis < 0 || instantMillis > MAX_INSTANT_IN_MILLIS) {
