211810b78405a595d77b128288c183ebcc4cdf77
Anders Rundgren <anders.rundgren.net@gmail.com>
2026-06-29 18:23:57 +0200

    android-cbor f16 bug fix

--- library/androidrefsrc/android/util/Half.java
+++ library/androidrefsrc/android/util/Half.java
@@ -1,7 +1,10 @@
 package android.util;
 
 public class Half {
-    public static float toHalf(float f) {
-        return f;
+    public static short toHalf(float f) {
+        return Float.floatToFloat16(f);
+    }
+    public static float toFloat(short half) {
+        return Float.float16ToFloat(half);
     }
 }
